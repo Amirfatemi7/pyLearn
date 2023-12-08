86456e951dccc2aabe8557eca2269b544d335446
Amirfatemi7 <7.amirfatemi@gmail.com>
2023-12-08 20:39:54 +0330

    .

--- assignment3/6_KMM.py
+++ assignment3/6_KMM.py
@@ -1,14 +1,3 @@
-# first = int(input('first:'))
-# second = int(input('second:'))
- 
-# if first > second:
-#   first, second = second, first
-# for i in range(1,first+1):
-#   if first%i == 0 and second%i == 0:
-#     gcd = i
-
-# lcm = (first*second)/gcd
-
 first = int(input('first:'))
 second = int(input('second:'))
  
